1ca74cfb8cba563e305783ad42baf2159243c5b8
Gilbert Montague <gilbert.i.montague@gmail.com>
2016-11-23 15:48:07 -0800

    autopep clean

--- joystick_uart/joystick_uart.py
+++ joystick_uart/joystick_uart.py
@@ -19,13 +19,16 @@ import serial
 import debug_messages as dm
 
 # Define some colors
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
+BLACK = (0,   0,   0)
+WHITE = (255, 255, 255)
 
 # This is a simple class that will help us print to the screen
 # It has nothing to do with the joysticks, just outputing the
 # information.
+
+
 class TextPrint:
+
     def __init__(self):
         self.reset()
         self.font = pygame.font.Font(None, 20)
@@ -34,19 +37,19 @@ class TextPrint:
         textBitmap = self.font.render(textString, True, BLACK)
         screen.blit(textBitmap, [self.x, self.y])
         self.y += self.line_height
-        
+
     def reset(self):
         self.x = 10
         self.y = 10
         self.line_height = 15
-        
+
     def indent(self):
         self.x += 10
-        
+
     def unindent(self):
         self.x -= 10
 
-#Serial init
+# Serial init
 default_port = '/dev/ttyUSB0'
 default_baud_rate = 115200
 
@@ -61,14 +64,14 @@ ser.isOpen()
 
 # Main script init
 pygame.init()
- 
+
 # Set the width and height of the screen [width,height]
 size = [500, 700]
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("My Game")
 
-#Loop until the user clicks the close button.
+# Loop until the user clicks the close button.
 is_running = True
 
 # Used to manage how fast the screen updates
@@ -76,7 +79,7 @@ clock = pygame.time.Clock()
 
 # Initialize the joysticks
 pygame.joystick.init()
-    
+
 # Get ready to print
 text_print = TextPrint()
 
@@ -91,10 +94,10 @@ disablePressed = False
 # -------- Main Program Loop -----------
 while is_running == True:
     # EVENT PROCESSING STEP
-    for event in pygame.event.get(): # User did something
+    for event in pygame.event.get():  # User did something
 
-        #Handle quits
-        if event.type == pygame.QUIT: 
+        # Handle quits
+        if event.type == pygame.QUIT:
             is_running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             is_running = False
@@ -106,46 +109,50 @@ while is_running == True:
     # Get count of joysticks
     joystick_count = pygame.joystick.get_count()
 
-    text_print.print_to_screen(screen, "Number of joysticks: {}".format(joystick_count))
+    text_print.print_to_screen(
+        screen, "Number of joysticks: {}".format(joystick_count))
     text_print.indent()
-    
+
     # For each joystick:
     for i in range(joystick_count):
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
-    
+
         text_print.print_to_screen(screen, "Joystick {}".format(i))
         text_print.indent()
-    
+
         # Get the name from the OS for the controller/joystick
         name = joystick.get_name()
         text_print.print_to_screen(screen, "Joystick name: {}".format(name))
 
-        #Since we are only controlling forward and backward control of motors, only read the value from axis 1
+        # Since we are only controlling forward and backward control of motors,
+        # only read the value from axis 1
         text_print.indent()
 
-        #Boolean for zero input
+        # Boolean for zero input
         axis = joystick.get_axis(1)
-        text_print.print_to_screen(screen, "Axis {} value: {:>6.3f}".format(1, axis))
-    
+        text_print.print_to_screen(
+            screen, "Axis {} value: {:>6.3f}".format(1, axis))
+
         if firstTime:
-            ser.write("Axis:" + str(axis) + '\r\n') 
+            ser.write("Axis:" + str(axis) + '\r\n')
             ser.flush()
             dm.print_info("Wrote axis:" + str(axis))
             lastAxis = axis
             firstTime = False
         elif axis != lastAxis:
-            ser.write("Axis:" + str(axis) + '\r\n') 
+            ser.write("Axis:" + str(axis) + '\r\n')
             ser.flush()
             dm.print_info("Wrote axis:" + str(axis))
             lastAxis = axis
 
-        #Button 10 Disable
+        # Button 10 Disable
         disable_button = joystick.get_button(10)
-        text_print.print_to_screen(screen, "Disable Button: {}".format(disable_button))
+        text_print.print_to_screen(
+            screen, "Disable Button: {}".format(disable_button))
 
-        # Not pressed and wasn't pressed 
-            # Do nothing
+        # Not pressed and wasn't pressed
+        # Do nothing
         # Not pressed but had been pressed (released)
         if (not disable_button) and disablePressed:
             disablePressed = False
@@ -157,12 +164,12 @@ while is_running == True:
             disablePressed = True
         # Pressed and had been pressed (held)
             # Do nothing
-        
-        #Button 11 Enable
+
+        # Button 11 Enable
         enable_button = joystick.get_button(11)
 
-        # Not pressed and wasn't pressed 
-            # Do nothing
+        # Not pressed and wasn't pressed
+        # Do nothing
         # Not pressed but had been pressed (released)
         if (not enable_button) and enablePressed:
             enablePressed = False
@@ -175,19 +182,19 @@ while is_running == True:
         # Pressed and had been pressed (held)
             # Do nothing
 
-        text_print.print_to_screen(screen, "Enable Button: {}".format(enable_button))
+        text_print.print_to_screen(
+            screen, "Enable Button: {}".format(enable_button))
 
-        text_print.unindent()        
+        text_print.unindent()
 
-    
     # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
-    
+
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
     # Limit to 60 frames per second
     clock.tick(60)
-    
+
 # Close the window and quit.
 # If you forget this line, the program will 'hang'
 # on exit if running from IDLE.
